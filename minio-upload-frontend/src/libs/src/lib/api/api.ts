7da326c1f58231c38ce5731c6cb645eebab41465
Gharantes <work.gharantes@gmail.com>
2025-08-05 16:25:38 -0300

    Api Lib

---
+++ minio-upload-frontend/src/libs/src/lib/api/api.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-api',
+  imports: [CommonModule],
+  templateUrl: './api.html',
+  styleUrl: './api.css',
+})
+export class Api {}
